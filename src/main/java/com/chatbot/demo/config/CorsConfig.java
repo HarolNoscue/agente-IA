ea9a3c305d13a15ea79d7580c5d036a6c109376a
HarolNoscue <hsnoscue@misena.edu.co>
2026-04-16 13:36:03 -0500

    se ajusta cors v2

--- src/main/java/com/chatbot/demo/config/CorsConfig.java
+++ src/main/java/com/chatbot/demo/config/CorsConfig.java
@@ -1,22 +1,16 @@
 package com.chatbot.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebFluxConfigurer {
 
-    @Bean
-    public WebFluxConfigurer corsConfigurer() {
-        return new WebFluxConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/chat")
-                        .allowedOrigins("*")
-                        .allowedMethods("POST");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
